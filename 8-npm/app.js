d85cd4133c182dc2db76437b4d0faf3aa028039d
hanasoke@gmail.com <hanasoke@gmail.com>
2024-12-14 17:55:24 +0700

    update folder 8-npm

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,11 @@
 const validator = require('validator');
+const chalk = require('chalk');
+
+// console.log(validator.isEmail('hanasoke@gmail.com'));
+// console.log(validator.isMobilePhone('085819536458', 'id-ID'));
+// console.log(validator.isNumeric('078234567g'));
+
+// console.log(chalk.bgYellow.white.italic('Hello World!'));
+const nama = 'Hanas';
+const pesan = chalk`Lorem, ipsum dolor {bgRed.black.strikethrough sit amet} constructur {bgMagentaBright.italic adipisicing} elit. Nama Saya : ${nama}`;
+console.log(pesan);
