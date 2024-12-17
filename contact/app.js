d610a554cb8971ed971eb0e45628df40906b18e9
hanasoke@gmail.com <hanasoke@gmail.com>
2024-12-17 15:25:58 +0700

    creating contact part 2

--- contact/app.js
+++ contact/app.js
@@ -1,11 +1,29 @@
+const yargs = require('yargs');
 const contacts = require('./contacts');
 
-const main = async () => {
-  const nama = await contacts.tulisPertanyaan('Masukkan nama anda : ');
-  const email = await contacts.tulisPertanyaan('Masukkan email anda : ');
-  const noHP = await contacts.tulisPertanyaan('Masukkan No HP anda : ');
+yargs.command({
+  command: 'add',
+  describe: 'Menambahkan contact baru',
+  builder: {
+    nama: {
+      describe: 'Nama lengkap',
+      demandOption: true,
+      type: 'string',
+    },
+    email: {
+      describe: 'Email',
+      demandOption: false,
+      type: 'string',
+    },
+    noHP: {
+      describe: 'Nomor Handphone',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+  },
+});
 
-  contacts.simpanContact(nama, email, noHP);
-};
-
-main();
+yargs.parse();
--- contact/contacts.js
+++ contact/contacts.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const chalk = require('chalk');
+const validator = require('validator');
 
 // membuat folder data
 const dirPath = './data';
@@ -18,26 +14,36 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-  return new Promise((resolve, reject) => {
-    rl.question(pertanyaan, (nama) => {
-      resolve(nama);
-    });
-  });
-};
-
 const simpanContact = (nama, email, noHP) => {
   const contact = { nama, email, noHP };
   const fileBuffer = fs.readFileSync('data/contacts.json', 'utf8');
   const contacts = JSON.parse(fileBuffer);
 
+  // cek duplikat
+  const duplikat = contacts.find((contact) => contact.nama === nama);
+  if (duplikat) {
+    console.log(chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain!'));
+    return false;
+  }
+
+  // cek email
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.inverse.bold('Email tidak valid'));
+      return false;
+    }
+  }
+
+  if (!validator.isMobilePhone(noHP, 'id-ID')) {
+    console.log(chalk.red.inverse.bold('Nomor HP tidak valid'));
+    return false;
+  }
+
   contacts.push(contact);
 
   fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
-  console.log('Terimakasih sudah memasukkan data.');
-
-  rl.close();
+  console.log(chalk.green.inverse.bold('terimakasih sudah memasukkan data.'));
 };
 
-module.exports = { tulisPertanyaan, simpanContact };
+module.exports = { simpanContact };
